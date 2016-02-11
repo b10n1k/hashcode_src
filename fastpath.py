a98176f7f6cfeee0f3dc64b63737958f4a956ec7
b10n1k <jbonatakis@gmail.com>
2016-02-11 20:17:39 +0000

    function to find the fastest root

--- fastpath.py
+++ fastpath.py
@@ -6,4 +6,21 @@ def getDistance(p, p2, p3, p4):
     dis = int(round(math.sqrt(point_one+point_two)))
     return dis
 
-print getDistance(2,3,4,5)
+def faster_root(nodes, distances, p, p2,):
+    unvisited = {node: None for node in nodes} #using None as +inf
+    visited = {}
+    current = (p,p2)
+    currentDistance = 0
+    unvisited[current] = currentDistance
+
+    while True:
+        for neighbour, distance in distances[current].items():
+            if neighbour not in unvisited: continue
+            newDistance = currentDistance + distance
+            if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
+                unvisited[neighbour] = newDistance
+        visited[current] = currentDistance
+        del unvisited[current]
+        if not unvisited: break
+        candidates = [node for node in unvisited.items() if node[1]]
+        current, currentDistance = sorted(candidates, key = lambda x: x[1])[0]
